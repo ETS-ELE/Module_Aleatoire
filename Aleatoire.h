29a4eec824b1c6336b6ca445131bf3a03b765b20
Samuel Geoffroy-Héroux <samuel.geoffroy.92@gmail.com>
2014-11-03 21:46:25 -0500

    Update Aleatoire.h

--- Aleatoire.h
+++ Aleatoire.h
@@ -13,6 +13,10 @@
 
 /*                                          DEFINE                                              */
 /************************************************************************************************/
+#ifndef MOD_ALEATOIRE
+#define MOD_ALEATOIRE
+
+//Les gars c'est quoi ça??
 #define nb_lignes_world_map 5
 #define nb_colonnes_world_map 4
 /************************************************************************************************/
@@ -27,5 +31,17 @@
 /*                                        DECLARATION                                           */
 /************************************************************************************************/
 void chemin_aleatoire_ajouter_un_pas(int* chemin[], int* longueur_chemin, int nb_lignes_grille, int nb_colonnes_grille);
+/*
+CHEMIN_ALEATOIRE_AJOUTER_UN_PAS
+        Cette fonction retourne la prochaine position à visiter à partir de la dernière position du chemin courant. 
+        La position est choisie au hasard et doit être valide. La nouvelle position ne doit donc pas sortir du tableau. 
+        Par exemple, pour le chemin présenté plus haut, la prochaine position est 16, 22 ou 20.
 
+	Parametre : 	int* chemin[]	--> Tableau contenant le labyrinthe à afficher.
+	                int* longueur_chemin
+	                
+	Return : 	Void.
+*/
+/************************************************************************************************/
 static int obtenir_prochaine_position(int chemin[],int longueur_chemin,int nb_lignes_grille,int nb_colonnes_grille);
+#endif
