bd912226dd55ede6193c238e6b9d83526024d5ac
FrancisMassey <francismasse_2@hotmail.com>
2014-11-05 20:09:30 -0500

    Update Aleatoire.h

--- Aleatoire.h
+++ Aleatoire.h
@@ -2,8 +2,8 @@
 //Projet   : Projet TP_2_Labyrinthe
 //Fichier  : Aleatoire.h
 //Contenu  : Contient les fonction relatif a la generation du labyrinthe
-//Objectif : Création d'un algorithme de réalisation de labyrinthe
-//Création : 1 novembre 2014
+//Objectif : Cr�ation d'un algorithme de r�alisation de labyrinthe
+//Cr�ation : 1 novembre 2014
 //                                                  Cree par Samuel - Link - Geoffroy-Heroux
 //                                                            Francis Masse
 //                                                            Christopher Dom
@@ -16,9 +16,7 @@
 #ifndef MOD_ALEATOIRE
 #define MOD_ALEATOIRE
 
-//Les gars c'est quoi ça??
-#define nb_lignes_world_map 5
-#define nb_colonnes_world_map 4
+
 /************************************************************************************************/
 
 /*                                         INCLUDES                                             */
@@ -33,32 +31,32 @@
 void chemin_aleatoire_ajouter_un_pas(int* chemin[], int* longueur_chemin, int nb_lignes_grille, int nb_colonnes_grille);
 /*
 CHEMIN_ALEATOIRE_AJOUTER_UN_PAS
-        Cette procédure ajoute un pas à un chemin et ajuste sa longueur. La position du prochain pas est choisie au hasard, 
-        mais doit être valide. Si la nouvelle position coupe le chemin existant, la boucle est tronquée. 
-        Observez le chemin présenté ici-bas.
+Cette proc�dure ajoute un pas � un chemin et ajuste sa longueur. La position du prochain pas est choisie au hasard,
+mais doit �tre valide. Si la nouvelle position coupe le chemin existant, la boucle est tronqu�e.
+Observez le chemin pr�sent� ici-bas.
 
-	Parametre : 	int* chemin[]		--> Pile contenant le chemin[] à trouver.
-	                int* longueur_chemin	--> Longueur de la pile chemin[].
-	                int nb_lignes_grille	--> Limite en i du labyrinthe.
-	                int nb_colonnes_grille	--> limite en j du labyrithe.
-	                
-	Return : 	Void.
+Parametre : 	int* chemin[]		--> Pile contenant le chemin[] � trouver.
+int* longueur_chemin	--> Longueur de la pile chemin[].
+int nb_lignes_grille	--> Limite en i du labyrinthe.
+int nb_colonnes_grille	--> limite en j du labyrithe.
+
+Return : 	Void.
 */
 /************************************************************************************************/
-static int obtenir_prochaine_position(int chemin[],int longueur_chemin,int nb_lignes_grille,int nb_colonnes_grille);
+static int obtenir_prochaine_position(int chemin[], int *longueur_chemin, int nb_lignes_grille, int nb_colonnes_grille);
 /*
 CHEMIN_ALEATOIRE_AJOUTER_UN_PAS
-        Cette fonction retourne la prochaine position à visiter à partir de la dernière position du chemin courant. 
-        La position est choisie au hasard et doit être valide. La nouvelle position ne doit donc pas sortir du tableau. 
-        Par exemple, pour le chemin présenté plus haut, la prochaine position est 16, 22 ou 20.
+Cette fonction retourne la prochaine position � visiter � partir de la derni�re position du chemin courant.
+La position est choisie au hasard et doit �tre valide. La nouvelle position ne doit donc pas sortir du tableau.
+Par exemple, pour le chemin pr�sent� plus haut, la prochaine position est 16, 22 ou 20.
 
-	Parametre : 	int* chemin[]		--> Pile contenant le chemin[] à trouver.
-	                int* longueur_chemin	--> Longueur de la pile chemin[].
-	                int nb_lignes_grille	--> Limite en i du labyrinthe.
-	                int nb_colonnes_grille	--> limite en j du labyrithe.
-	                
-	Return : 	int Prochaine position valide.
+Parametre : 	int* chemin[]		--> Pile contenant le chemin[] � trouver.
+int* longueur_chemin	--> Longueur de la pile chemin[].
+int nb_lignes_grille	--> Limite en i du labyrinthe.
+int nb_colonnes_grille	--> limite en j du labyrithe.
+
+Return : 	int Prochaine position valide.
 */
 /************************************************************************************************/
 
-        #endif
+#endif
